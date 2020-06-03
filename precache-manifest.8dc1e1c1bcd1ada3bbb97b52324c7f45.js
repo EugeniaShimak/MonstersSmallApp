b633f7b56dccfbdcb1d41c8847c8ffaad022b1e5
EugeniaShimak <greenlighty24@gmail.com>
2020-06-03 21:20:07 +0500

    Updates

---
+++ precache-manifest.8dc1e1c1bcd1ada3bbb97b52324c7f45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34e053d23379e12b30eb12c8e92a4352",
+    "url": "/MonstersSmallApp/index.html"
+  },
+  {
+    "revision": "c7970574ee70e243ad3c",
+    "url": "/MonstersSmallApp/static/css/main.d0f2b9df.chunk.css"
+  },
+  {
+    "revision": "80de6e261c3a1f36230c",
+    "url": "/MonstersSmallApp/static/js/2.05463ef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonstersSmallApp/static/js/2.05463ef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7970574ee70e243ad3c",
+    "url": "/MonstersSmallApp/static/js/main.9fc87532.chunk.js"
+  },
+  {
+    "revision": "00692ae152ff541c0516",
+    "url": "/MonstersSmallApp/static/js/runtime-main.343c922f.js"
+  }
+]);
